0912d3c5716c7565c8322d716d59852651209ec3
lashamorgoshia <xnagamer@live.com>
2024-01-05 00:10:41 +0400

    დაემატა Startup კონფიგურაცია JWT Token ის მიღების

--- IdentityServer4Client/Program.cs
+++ IdentityServer4Client/Program.cs
@@ -1,10 +1,41 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using IdentityServer4.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddIdentityServer()
+    .AddDeveloperSigningCredential()
+    .AddInMemoryApiResources(new List<ApiResource>
+    {
+        new ApiResource("api1", "My API")
+    })
+    .AddInMemoryClients(new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+            AllowedScopes = { "api1" }
+        }
+    });
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://localhost:5000";
+        options.RequireHttpsMetadata = false;
+        options.Audience = "api1";
+    });
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
